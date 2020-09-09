3b6d335d100712af8701d4b7f0b8d442f40deb91
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-09-09 16:55:30 +0200

    Embed the shop

---
+++ pages/shop.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <BlockHeaderPlain title="Shop">
+            <template v-slot:side-content>
+            </template>
+        </BlockHeaderPlain>
+          <div id="shop">The JupyterCon shop is loading...</div>
+        <DesktopFooter />
+    </div>
+</template>
+
+<script>
+    import BlockHeaderPlain from "../components/BlockHeaderPlain";
+    import DesktopFooter from "../components/DesktopFooter";
+
+    export default {
+        name: "shop",
+        components: {DesktopFooter, BlockHeaderPlain},
+    }
+</script>
